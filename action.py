3b7df6cd29cdd8f8049e945d5188d015897277df
Kasia Matejak <katarzyna.k.matejak@gmail.com>
2022-07-28 22:14:15 +0200

    feat: replace variables with classes

--- action.py
+++ action.py
@@ -1,5 +1,10 @@
 import pygame
 from pygame.locals import *
+from characters import Wizzard, Knight
+
+# tworzę postaci
+knight = Knight()
+wizzard = Wizzard()
 
 pygame.init()
 window = pygame.display.set_mode((500, 500))
@@ -9,52 +14,49 @@ run = True
 background = pygame.image.load("background.jpg")
 height = 50
 width = 50
-w_position_x=220
-w_position_y=1
-k_position_x=250
-k_position_y=450
-
-#wizzard_image = pygame.image.load("wizzard.png")
-#knight_image = pygame.image.load("knight.png")
-#wizzard_location = wizzard_image.get_rect()
-#wizzard_location = w_position_x, w_position_y
-wizzard = pygame.Rect(w_position_x, w_position_y, width, height)
-knight = pygame.Rect(k_position_x, k_position_y, width, height)
 
 while run:
     pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-           run = False
+            run = False
+        elif wizzard.check_if_alive() == False:
+            print("Knight has killed wizzard. Long live the king!")
+            run = False
+        elif knight.check_if_alive() == False:
+            print("Wizzard has killed the knight. Let there be anarchy")
+            run = False
         elif event.type == KEYDOWN:
             if event.key == K_SPACE:
-                print("Fight WIZZARD")
+                wizzard.attack(knight)
             elif event.key == K_RETURN:
-                print("Fight KNIGHT!")
+                knight.attack(wizzard)
 
-    window.blit(background, (0,0))
-    #window.blit(wizzard_image, wizzard_location)
-    pygame.draw.rect(window, (153, 204, 255), wizzard)
-    pygame.draw.rect(window, (102, 0, 51), knight)
-    
-    #poruszanie się czarodzieja
-    if pygame.key.get_pressed() [pygame.K_w]:
-        wizzard.y -= 1
-    if pygame.key.get_pressed() [pygame.K_a]:
-        wizzard.x -= 1
-    if pygame.key.get_pressed() [pygame.K_s]:
-        wizzard.y += 1
-    if pygame.key.get_pressed() [pygame.K_d]:
-        wizzard.x += 1
+    wizzard_icon = pygame.Rect(wizzard.position_x, wizzard.position_y, width, height)
+    knight_icon = pygame.Rect(knight.position_x, knight.position_y, width, height)
 
-    #poruszanie się rycerza
-    if pygame.key.get_pressed() [pygame.K_UP]:
-        knight.y -= 1
-    if pygame.key.get_pressed() [pygame.K_LEFT]:
-        knight.x -= 1
-    if pygame.key.get_pressed() [pygame.K_DOWN]:
-        knight.y += 1
-    if pygame.key.get_pressed() [pygame.K_RIGHT]:
-        knight.x += 1
-        
+    window.blit(background, (0, 0))
+    pygame.draw.rect(window, (51, 102, 0), wizzard_icon)
+    pygame.draw.rect(window, (102, 0, 51), knight_icon)
+
+    # poruszanie się czarodzieja - wasd, spacja
+    if pygame.key.get_pressed()[pygame.K_w]:
+        wizzard.move_up()
+    if pygame.key.get_pressed()[pygame.K_a]:
+        wizzard.move_left()
+    if pygame.key.get_pressed()[pygame.K_s]:
+        wizzard.move_down()
+    if pygame.key.get_pressed()[pygame.K_d]:
+        wizzard.move_right()
+
+    # poruszanie się rycerza - strzałki, enter
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        knight.move_up()
+    if pygame.key.get_pressed()[pygame.K_LEFT]:
+        knight.move_left()
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        knight.move_down()
+    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        knight.move_right()
+
     pygame.display.update()
--- characters.py
+++ characters.py
@@ -1,78 +1,97 @@
 import random
 import math
 
-#klasy postaci zawierają atrybuty pozycji postaci
+# klasy postaci zawierają atrybuty pozycji postaci
 class Character:
-    def __init__(self, name, fighting_technique, damage, position_x, position_y, life=100):
+    def __init__(
+        self, name, fighting_technique, damage, position_x, position_y, life=100
+    ):
         self.name = name
         self.fighting_technique = fighting_technique
         self.damage = damage
         self.life = life
         self.position_x = position_x
         self.position_y = position_y
-  
-    #metoda zawierania obrażeń
+
+    # metoda zawierania obrażeń
     def get_damage(self, damage):
         self.life = self.life - damage
 
-    #metoda walki - zawiera w sobie zadawanie obrażeń
+    # metoda walki - zawiera w sobie zadawanie obrażeń
     def fight(self, opponent):
         print(self.name + self.fighting_technique)
         opponent.get_damage(self.damage)
 
-    #metoda kalkulowania dystansu pomiędzy przeciwnikami
+    # metoda kalkulowania dystansu pomiędzy przeciwnikami
     def calculate_distance(self, opponent):
-        xy = math.sqrt((self.position_x-opponent.position_x)**2+(self.position_y-opponent.position_y)**2)
-        return xy
+        distance = math.sqrt(
+            (self.position_x - opponent.position_x) ** 2
+            + (self.position_y - opponent.position_y) ** 2
+        )
+        return distance
 
-    #metoda ataku - zawiera w sobie walkę i tym samym zadawanie obrażeń oraz kalkulację dystansu
+    # metoda ataku - zawiera w sobie walkę i tym samym zadawanie obrażeń oraz kalkulację dystansu
     def attack(self, opponent):
-        if self.calculate_distance(opponent) <3:
+        if self.calculate_distance(opponent) < 100:
             self.fight(opponent)
+            print(opponent.name, "has ", opponent.life, " life left")
         else:
             print("Za daleko!")
 
-    #metoda sprawdzania czy postać ma jeszcze punkty życia
+    # metoda sprawdzania czy postać ma jeszcze punkty życia
     def check_if_alive(self):
-        if self.life >0:
+        if self.life > 0:
             return True
         else:
             return False
-    
-#klasa dzieciczącą z postaci - czarodziej
-#czarodziej zadaje zawsze obrażenia, ale nieduże
+
+    # def check_if_on_board(self, position):
+    # if position >0 and position <500:
+    # print("You can't go there")
+
+    # metody poruszania się
+    def move_up(self):
+        self.position_y -= 1
+
+    def move_down(self):
+        self.position_y += 1
+
+    def move_right(self):
+        self.position_x += 1
+
+    def move_left(self):
+        self.position_x -= 1
+
+
+# klasa dzieciczącą z postaci - czarodziej
+# czarodziej zadaje zawsze obrażenia, ale nieduże
 class Wizzard(Character):
     def __init__(self):
-        super().__init__(name='Wizzard', fighting_technique=" casted a spell", damage=10, position_x=1, position_y=1)
+        super().__init__(
+            name="Wizzard",
+            fighting_technique=" casted a spell",
+            damage=10,
+            position_x=220,
+            position_y=1,
+        )
 
 
-#klasa dzieciczącą z postaci - rycerz
-#rycerz zadaje znaczne obrażenia albo nie zadaje wcale (czarodziej blokuje go zaklęciem)
+# klasa dzieciczącą z postaci - rycerz
+# rycerz zadaje znaczne obrażenia albo nie zadaje wcale (czarodziej blokuje go zaklęciem)
 class Knight(Character):
     def __init__(self):
-        super().__init__(name="Knight", fighting_technique= " stabbed with a sword", damage=20, position_x=3, position_y=9)
+        super().__init__(
+            name="Knight",
+            fighting_technique=" stabbed with a sword",
+            damage=20,
+            position_x=220,
+            position_y=450,
+        )
+
     def fight(self, opponent):
         print("Knight makes an attempt")
-        x = random.randint(0,1)
+        x = random.randint(0, 1)
         if x == 1:
             super().fight(opponent)
         else:
             print("Wizzard used protection spell")
-
-#tworzę postaci
-wizzard = Wizzard()
-knight = Knight()
-
-#tworzę funckję walki pomiędzy postaciami 
-def battle():
-    #warunkiem rozpoczęcia ataku jest pozostawanie przy życiu
-    while wizzard.check_if_alive() and knight.check_if_alive():
-        knight.attack(wizzard)
-        wizzard.attack(knight)
-    else:
-        if wizzard.check_if_alive() == False:
-            print("Knight has killed wizzard. Long live the king!")
-        else:
-            print("Wizzard has killed the knight. Let there be anarchy")
-
-battle()
